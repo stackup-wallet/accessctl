ab6053725ec2af34b0388aa5ecd309049b93c13c
hazim-j <hazim.jumali@gmail.com>
2024-07-30 20:17:32 +1000

    Fix broken reinstall test case

--- test/Config.t.sol
+++ test/Config.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.23;
 
+import { AccountType } from "modulekit/test/RhinestoneModuleKit.sol";
 import { TestHelper } from "test/TestHelper.sol";
 import { IAMValidator } from "src/IAMValidator.sol";
 import { Signer, SignerLib } from "src/Signer.sol";
@@ -18,52 +19,53 @@ contract ConfigTest is TestHelper {
         assertEq(target.balance, prevBalance + value);
     }
 
-    // TODO: fix below for Safe and Kernel test cases
     function testReinstallResetsState() public {
-        // _execUserOp(
-        //     address(validator),
-        //     0,
-        //     abi.encodeWithSelector(
-        //         IAMValidator.addSigner.selector, dummyP256PubKeyX1, dummyP256PubKeyY1
-        //     )
-        // );
-        // _execUserOp(
-        //     address(validator),
-        //     0,
-        //     abi.encodeWithSelector(
-        //         IAMValidator.addSigner.selector, dummyP256PubKeyX2, dummyP256PubKeyY2
-        //     )
-        // );
-        // _uninstallModule();
-        // assertTrue(validator.getSigner(address(instance.account), rootSignerId).isNull());
-        // assertTrue(validator.getSigner(address(instance.account), rootSignerId + 1).isNull());
-        // assertTrue(validator.getSigner(address(instance.account), rootSignerId + 2).isNull());
+        // TODO: Remove this once modulekit updates Kernel to v3.1.
+        // Kernel 3.0 has an issue that causes this test to fail.
+        if (instance.accountType == AccountType.KERNEL) return;
 
-        // _installModule();
-        // _execUserOp(
-        //     address(validator),
-        //     0,
-        //     abi.encodeWithSelector(
-        //         IAMValidator.addSigner.selector, dummyP256PubKeyX1, dummyP256PubKeyY1
-        //     )
-        // );
-        // _execUserOp(
-        //     address(validator),
-        //     0,
-        //     abi.encodeWithSelector(
-        //         IAMValidator.addSigner.selector, dummyP256PubKeyX2, dummyP256PubKeyY2
-        //     )
-        // );
-        // assertTrue(
-        //     validator.getSigner(address(instance.account), rootSignerId).isEqual(dummyRootSigner)
-        // );
-        // assertTrue(
-        //     validator.getSigner(address(instance.account), rootSignerId +
-        // 1).isEqual(dummySigner1)
-        // );
-        // assertTrue(
-        //     validator.getSigner(address(instance.account), rootSignerId +
-        // 2).isEqual(dummySigner2)
-        // );
+        _execUserOp(
+            address(validator),
+            0,
+            abi.encodeWithSelector(
+                IAMValidator.addSigner.selector, dummyP256PubKeyX1, dummyP256PubKeyY1
+            )
+        );
+        _execUserOp(
+            address(validator),
+            0,
+            abi.encodeWithSelector(
+                IAMValidator.addSigner.selector, dummyP256PubKeyX2, dummyP256PubKeyY2
+            )
+        );
+        _uninstallModule();
+        assertTrue(validator.getSigner(address(instance.account), rootSignerId).isNull());
+        assertTrue(validator.getSigner(address(instance.account), rootSignerId + 1).isNull());
+        assertTrue(validator.getSigner(address(instance.account), rootSignerId + 2).isNull());
+
+        _installModule();
+        _execUserOp(
+            address(validator),
+            0,
+            abi.encodeWithSelector(
+                IAMValidator.addSigner.selector, dummyP256PubKeyX1, dummyP256PubKeyY1
+            )
+        );
+        _execUserOp(
+            address(validator),
+            0,
+            abi.encodeWithSelector(
+                IAMValidator.addSigner.selector, dummyP256PubKeyX2, dummyP256PubKeyY2
+            )
+        );
+        assertTrue(
+            validator.getSigner(address(instance.account), rootSignerId).isEqual(dummyRootSigner)
+        );
+        assertTrue(
+            validator.getSigner(address(instance.account), rootSignerId + 1).isEqual(dummySigner1)
+        );
+        assertTrue(
+            validator.getSigner(address(instance.account), rootSignerId + 2).isEqual(dummySigner2)
+        );
     }
 }
