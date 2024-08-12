3f6da64f719152b6f086e2d7ff0d4d32222460ff
hazim-j <hazim.jumali@gmail.com>
2024-08-12 20:48:55 +1000

    test

--- test/Utils.t.sol
+++ test/Utils.t.sol
@@ -36,4 +36,47 @@ contract UtilTest is TestHelper {
         assertEq(policyId, rootPolicyId + 2);
         assertEq(actionId, rootActionId + 2);
     }
+
+    function testIdsOnlyGoUp() public {
+        // Add entities
+        _execUserOp(
+            address(module),
+            0,
+            abi.encodeWithSelector(IAMModule.addWebAuthnSigner.selector, dummySigner1)
+        );
+        _execUserOp(
+            address(module),
+            0,
+            abi.encodeWithSelector(IAMModule.addPolicy.selector, dummy1EtherSinglePolicy)
+        );
+        _execUserOp(
+            address(module),
+            0,
+            abi.encodeWithSelector(IAMModule.addAction.selector, dummySendMax1EtherAction)
+        );
+
+        // Remove entities
+        _execUserOp(
+            address(module),
+            0,
+            abi.encodeWithSelector(IAMModule.removeSigner.selector, rootSignerId + 1)
+        );
+        _execUserOp(
+            address(module),
+            0,
+            abi.encodeWithSelector(IAMModule.removePolicy.selector, rootPolicyId + 1)
+        );
+        _execUserOp(
+            address(module),
+            0,
+            abi.encodeWithSelector(IAMModule.removeAction.selector, rootActionId + 1)
+        );
+
+        // Asser next ids will still go up
+        (uint112 signerId, uint112 policyId, uint48 actionId) =
+            module.getNextIds(address(instance.account));
+        assertEq(signerId, rootSignerId + 2);
+        assertEq(policyId, rootPolicyId + 2);
+        assertEq(actionId, rootActionId + 2);
+    }
 }
