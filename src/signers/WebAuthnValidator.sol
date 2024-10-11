c32bd02fb34bd2deaba033fa541631f1f8e12cfb
hazim <hazim.jumali@gmail.com>
2024-10-11 17:43:09 +1100

    Align with smart sessions cantina-spearbit-remediated branch (#64)

--- src/signers/WebAuthnValidator.sol
+++ src/signers/WebAuthnValidator.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.23;
 
+import { ERC7579_MODULE_TYPE_STATELESS_VALIDATOR } from "smart-sessions/DataTypes.sol";
 import { ISessionValidator } from "smart-sessions/interfaces/ISessionValidator.sol";
 import { P256Credentials, P256CredentialsLib } from "src/signers/P256Credentials.sol";
 
@@ -20,7 +21,15 @@ contract WebAuthnValidator is ISessionValidator {
         return P256Credentials(x, y).verifyWebAuthnSignature(hash, sig);
     }
 
-    function supportsInterface(bytes4 interfaceID) external pure returns (bool) {
-        return interfaceID == WebAuthnValidator.validateSignatureWithData.selector;
+    function isModuleType(uint256 id) external pure returns (bool) {
+        return id == ERC7579_MODULE_TYPE_STATELESS_VALIDATOR;
+    }
+
+    function onInstall(bytes calldata data) external override { }
+
+    function onUninstall(bytes calldata data) external override { }
+
+    function isInitialized(address) external pure override returns (bool) {
+        return true;
     }
 }
